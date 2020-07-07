da58cb49a977e4d70f718ba5823f50d421295243
deenuy <deenuy@gmail.com>
2020-07-07 17:26:07 -0400

    if applied, this commit will add highscores page to view user scores

---
+++ scripts/highscores.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+    // Function to return to home page
+    $("#button-return").click(function(){
+
+        //this will redirect us in same window
+        document.location.href = "index.html";
+        //window.open(go_to_url, '_blank');
+    })
+}
